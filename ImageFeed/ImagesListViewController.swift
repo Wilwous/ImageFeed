66b458c9ab4a3efaf6b24b18164dacb764e1743a
wilwous <wilwous@gmail.com>
2023-08-02 15:07:44 +0300

    Добавил расширения к классу и цвета.

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
+    
+    @IBOutlet private var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
+
+    extension ImagesListViewController: UITableViewDelegate {
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    }
+    
+    extension ImagesListViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+    }
 
