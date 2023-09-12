94c6d1a45d0b8b02b3d4f03355de50cdb6e2a9a6
wilwous <wilwous@gmail.com>
2023-09-12 16:31:04 +0300

    Реализовал зум.

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -17,13 +17,22 @@ final class SingleImageViewController: UIViewController {
     }
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet private weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
 
     @IBAction func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
