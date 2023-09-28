7d23e34109e4e0a670edd95114e6d50353dbf3cb
wilwous <wilwous@gmail.com>
2023-09-28 12:36:58 +0300

    Добавил URLComponents и добавил скругление кнопки войти

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -11,4 +11,21 @@ import WebKit
 final class WebViewViewController: UIViewController{
     
     @IBOutlet private weak var webView: WKWebView!
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/token"
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
