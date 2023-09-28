d4edd1de7d568ba3fbc9dec1b0722d36b7d3a49b
wilwous <wilwous@gmail.com>
2023-09-28 10:49:50 +0300

    Сверстал WebViewViewController

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,4 +5,10 @@
 //  Created by Антон Павлов on 28.09.2023.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController{
+    
+    @IBOutlet private weak var webView: WKWebView!
+}
