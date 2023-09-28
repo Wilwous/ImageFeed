393b212c953e346f36f7d71842344bcb4ba6eeff
wilwous <wilwous@gmail.com>
2023-09-28 14:10:44 +0300

    Исправил ошибку

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -16,13 +16,13 @@ final class WebViewViewController: UIViewController{
     }
     
     override func viewDidLoad() {
-        let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/token"
+        let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
         var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
         urlComponents.queryItems = [
             URLQueryItem(name: "client_id", value: AccessKey),
             URLQueryItem(name: "redirect_uri", value: RedirectURI),
             URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: AccessScope)
+            URLQueryItem(name: "scope", value: AccessScope),
         ]
         let url = urlComponents.url!
         let request = URLRequest(url: url)
