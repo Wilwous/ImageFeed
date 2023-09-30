3d36259783e4f6469c55e50fed5e45e00a546e67
wilwous <wilwous@gmail.com>
2023-09-30 22:06:28 +0300

    Добавлен OAuth2Service для запроса токена доступа

---
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -0,0 +1,8 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Created by Антон Павлов on 30.09.2023.
+//
+
+import Foundation
---
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -0,0 +1,8 @@
+//
+//  OAuth2TokenStorage.swift
+//  ImageFeed
+//
+//  Created by Антон Павлов on 30.09.2023.
+//
+
+import Foundation
