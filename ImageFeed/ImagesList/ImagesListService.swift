4239f8fd7221bd52c37c97fdb635e9ad06ac9db6
Wilwous <Mick>
2023-12-14 18:48:05 +0300

    Добавлены структуры для декодинга JSON-ответа

---
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -0,0 +1,11 @@
+//
+//  ImagesListService.swift
+//  ImageFeed
+//
+//  Created by Антон Павлов on 12.12.2023.
+//
+
+import Foundation
+
+final class ImagesListService {
+}
---
+++ ImageFeed/ImagesList/ImagesListStructures.swift
@@ -0,0 +1,38 @@
+//
+//  ImagesListStructures.swift
+//  ImageFeed
+//
+//  Created by Антон Павлов on 12.12.2023.
+//
+
+import Foundation
+
+struct PhotoResult: Decodable {
+    let id: String
+    let createdAt: String?
+    let width: Int?
+    let height: Int?
+    let isLiked: Bool?
+    let description: String?
+    let urls: UrlsResult?
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case createdAt = "created_at"
+        case width = "width"
+        case height = "height"
+        case isLiked = "liked_by_user"
+        case description = "description"
+        case urls = "urls"
+    }
+}
+
+struct UrlsResult: Decodable {
+    let trumbImageURL: String?
+    let fullImageURL: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case trumbImageURL = "thumb"
+        case fullImageURL = "full"
+    }
+}
