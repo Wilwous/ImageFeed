afe7236506b747066aa186b9b28356537f6eb5d9
Wilwous <Mick>
2023-12-26 14:31:55 +0300

    Добавлены Unit-тесты для ProfileView

--- ImageFeed/ServiceAccessLayer/OAuth2TokenStorage.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2TokenStorage.swift
@@ -13,8 +13,6 @@ final class OAuth2TokenStorage {
     static let shared = OAuth2TokenStorage()
     private let keychain = KeychainWrapper.standard
     
-    private init() {}
-    
     var token: String? {
         get {
             KeychainWrapper.standard.string(forKey: "bearerToken")
